ed7f8870ca310ba9589efe0375567edf7c065ad8
lucas_almeida <lucas.almdiniz@gmail.com>
2022-04-22 16:53:00 -0300

    Updates

---
+++ precache-manifest.2b8596132a8e1a9b7ec5991f3f55d9ec.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12238ea0355acc4d08b3cc582e6576a4",
+    "url": "/Trybe-Projeto_19-Star_Wars_Planets_Search/index.html"
+  },
+  {
+    "revision": "f1216ed0449b65c1f5a5",
+    "url": "/Trybe-Projeto_19-Star_Wars_Planets_Search/static/css/main.4fc17f92.chunk.css"
+  },
+  {
+    "revision": "9390d434eee1e05213ea",
+    "url": "/Trybe-Projeto_19-Star_Wars_Planets_Search/static/js/2.477e8ed5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Trybe-Projeto_19-Star_Wars_Planets_Search/static/js/2.477e8ed5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1216ed0449b65c1f5a5",
+    "url": "/Trybe-Projeto_19-Star_Wars_Planets_Search/static/js/main.d8860561.chunk.js"
+  },
+  {
+    "revision": "5e3fd2af02133dd5bfea",
+    "url": "/Trybe-Projeto_19-Star_Wars_Planets_Search/static/js/runtime-main.a2faae1e.js"
+  },
+  {
+    "revision": "111452fff6aaac634f9867b821115443",
+    "url": "/Trybe-Projeto_19-Star_Wars_Planets_Search/static/media/lixeira.111452ff.png"
+  },
+  {
+    "revision": "cae276440cad47e06d002d8cc07b376b",
+    "url": "/Trybe-Projeto_19-Star_Wars_Planets_Search/static/media/starwars-background.cae27644.png"
+  },
+  {
+    "revision": "7e737fa8ee54962244ccac83ce11298b",
+    "url": "/Trybe-Projeto_19-Star_Wars_Planets_Search/static/media/starwars-fight.7e737fa8.gif"
+  },
+  {
+    "revision": "a89ec600216b6a0b3b6efc5a9e7dd590",
+    "url": "/Trybe-Projeto_19-Star_Wars_Planets_Search/static/media/starwars-logo.a89ec600.gif"
+  },
+  {
+    "revision": "fe9fa9f1ba73ca316504afc88c1d236e",
+    "url": "/Trybe-Projeto_19-Star_Wars_Planets_Search/static/media/starwars-run-robo.fe9fa9f1.gif"
+  },
+  {
+    "revision": "6ba45665591f30ab1ad0c3f7f264d0ca",
+    "url": "/Trybe-Projeto_19-Star_Wars_Planets_Search/static/media/wave-haikei(4).6ba45665.svg"
+  }
+]);
